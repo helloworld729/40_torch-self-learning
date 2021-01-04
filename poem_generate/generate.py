9fa420ef260b6506515f51ff28dc2a337414c47f
knight <1952933329@qq.com>
2021-01-04 10:43:06 +0800

    修改generate文件为多加载多模型

--- poem_generate/generate.py
+++ poem_generate/generate.py
@@ -1,23 +1,22 @@
 ''' Translate input text with trained model. '''
 from tqdm import tqdm
-import torch, torch.utils.data, argparse
-# from transformer.Translator import Translator
-from transformer.My_Translator import Translator
+import torch, torch.utils.data, argparse, os
 from dataset import collate_fn, TranslationDataset
 from preprocess import read_instances_from_file, convert_instance_to_idx_seq
 
 def main(author=False):
+    if author:from transformer.Translator import Translator
+    else:from transformer.My_Translator import Translator
     parser = argparse.ArgumentParser(description='generate.py')
-
-    parser.add_argument('-model', default='weights/transformer_accu_38.785.chkpt',
-                        help='Path to model .pt file')
+    # parser.add_argument('-model', default='weights/transformer_accu_38.785.chkpt',
+    #                     help='Path to model .pt file')
     parser.add_argument('-src', default='data/test.formmer',
                         help='Source sequence to decode (one line per sequence)')
     parser.add_argument('-vocab', default='data/save_file/file_saved.txt',
                         help='Source sequence to decode (one line per sequence)')
-    parser.add_argument('-output', default='data/pred2.txt',  # 翻译结果保存文件路径
-                        help="""Path to output the predictions (each line will
-                        be the decoded sequence""")
+    # parser.add_argument('-output', default='data/pred2.txt',  # 翻译结果保存文件路径
+    #                     help="""Path to output the predictions (each line will
+    #                     be the decoded sequence""")
     parser.add_argument('-beam_size', type=int, default=2,
                         help='Beam size')
     parser.add_argument('-batch_size', type=int, default=2,
@@ -28,17 +27,22 @@ def main(author=False):
     parser.add_argument('-no_cuda', action='store_true')
 
     opt = parser.parse_args()
-    # opt.cuda = not opt.no_cuda
-    opt.cuda = False
+    opt.cuda = True if torch.cuda.is_available() else False
+
     # Prepare DataLoader
     preprocess_data = torch.load(opt.vocab)
-    preprocess_settings = preprocess_data['settings']  # 保存有长度数据，文件位置信息
+
+    # 保存有长度数据，文件位置信息
+    preprocess_settings = preprocess_data['settings']
     max_trans_len = preprocess_settings.max_word_seq_len
-    test_src_word_insts = read_instances_from_file(    # 原始数据list
+
+    # 原始数据list
+    test_src_word_insts = read_instances_from_file(
         opt.src,
         max_trans_len,
         preprocess_settings.keep_case)
-    test_src_insts = convert_instance_to_idx_seq(      # word-->index
+    # 将句子序列转化为
+    test_src_insts = convert_instance_to_idx_seq(
         test_src_word_insts, preprocess_data['dict']['src'])
 
     test_loader = torch.utils.data.DataLoader(
@@ -50,21 +54,30 @@ def main(author=False):
         batch_size=opt.batch_size,
         collate_fn=collate_fn)
 
-    translator = Translator(opt)
+    # 遍历所有的模型文件
+    for check_point in os.listdir("weights"):
+        opt.model = "weights/" + check_point
+        model_name = opt.model.split("_")[-1]
+        # 设定对应的输出目录
+        opt.output = 'data/'+ model_name + ".txt"
+        # 加载模型
+        translator = Translator(opt)
 
-    with open(opt.output, 'w', encoding='utf-8') as f:
-        for batch in tqdm(test_loader, mininterval=2, desc='  - (Test)', leave=False):
-            # batch是批量数据(单侧待翻译数据index列表) + 位置索引, *使得参数列表化
-            # (列表有两部分内容，1是seq的id序列，2是对应的未知序列)
-            all_hyp, all_scores = translator.translate_batch(*batch)
-            for idx_seqs in all_hyp:
-                if not author:
-                    pred_line = ''.join([test_loader.dataset.tgt_idx2word[idx] for idx in idx_seqs])  # RBX
-                else:
-                    idx_seqs = idx_seqs[0]
-                    pred_line = ''.join([test_loader.dataset.tgt_idx2word[idx] for idx in idx_seqs][0])
-                f.write(pred_line.replace("\n", "") + "\n")
-    print('[Info] Finished.')
+        with open(opt.output, 'w', encoding='utf-8') as f:
+            # leave设置为False，是的在一行持续输出
+            for batch in tqdm(test_loader, mininterval=2, desc='  - (Test)', leave=False):
+                # batch是批量数据(单侧待翻译数据index列表) + 位置索引, *使得参数列表化
+                # (列表有两部分内容，1是seq的id序列，2是对应的未知序列)
+                all_hyp, all_scores = translator.translate_batch(*batch)
+                for i in range(len(all_hyp)):
+                    idx_seqs = all_hyp[i]
+                    idx2word = test_loader.dataset.tgt_idx2word
+                    if not author: pred_line = ''.join([idx2word[idx] for idx in idx_seqs])
+                    else:          pred_line = ''.join([idx2word[idx] for idx in idx_seqs[0]][0])
+                    formmer = ''.join([idx2word[idx] for idx in batch[i][1].data.numpy()]).replace("\n", "")
+                    latter = pred_line.replace("\n", "")
+                    f.write(formmer + "--->" + latter + "\n\n")
+        print('[Info] Finished.')
 
 if __name__ == "__main__":
     main()
