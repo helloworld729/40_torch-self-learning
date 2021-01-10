2b4eb449c762b567ca7a203223f30bb9519c29ab
Knight <1952933329@qq.com>
2021-01-10 23:35:56 +0800

    UPdate

--- summationModel/summarization_prepareNet/transFormerOrigin/data/data_help.py
+++ summationModel/summarization_prepareNet/transFormerOrigin/data/data_help.py
@@ -26,11 +26,15 @@ def parse(file_path, write_name):
     former.close()
     latter.close()
 
+print("INFO: begin")
+print("INFO: train")
 file_path = "PART_I.txt"
 parse(file_path, "train")
-# file_path = "PART_II.txt"
-# parse(file_path, "valid")
-# file_path = "PART_III.txt"
-# parse(file_path, "text")
+print("INFO: valid")
+file_path = "PART_II.txt"
+parse(file_path, "valid")
+print("INFO: test")
+file_path = "PART_III.txt"
+parse(file_path, "test")
 
 # 村民大喊“潮水来了”<br/>他们戴着耳机没听见
