efdf70e95c62276f0198759fc56e943be8b9b9b2
knight <1952933329@qq.com>
2021-01-11 09:23:57 +0800

    UPdate

--- summationModel/summarization_prepareNet/transFormerOrigin/data/data_help.py
+++ summationModel/summarization_prepareNet/transFormerOrigin/data/data_help.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 def parse(file_path, write_name):
     text = open("DATA/" + file_path, encoding="UTF-8")
-    soup = BeautifulSoup(text, features="lxml").find_all("doc")
+    soup = BeautifulSoup(text, features="html.parser").find_all("doc")
 
     former = open("./" + write_name + ".former.txt", mode="w", encoding="UTF-8")
     latter = open("./" + write_name + ".latter.txt", mode="w", encoding="UTF-8")
@@ -31,9 +31,9 @@ print("INFO: train")
 file_path = "PART_I.txt"
 parse(file_path, "train")
 print("INFO: valid")
-file_path = "PART_II.txt"
-parse(file_path, "valid")
-print("INFO: test")
-file_path = "PART_III.txt"
-parse(file_path, "test")
+# file_path = "PART_II.txt"
+# parse(file_path, "valid")
+# print("INFO: test")
+# file_path = "PART_III.txt"
+# parse(file_path, "test")
 
